e94635b205a4e96e2fa102c26f4e27206d4d42ac
SeedPaths-User <lessdismissive@email.com>
2014-10-17 13:05:37 -0600

    blogging

---
+++ MyBlog/MyBlog/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyBlog.Models;
+
+namespace MyBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        BlogEntities db = new BlogEntities();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(db.Posts.OrderByDescending(x => x.DateCreate));
+        }
+
+        public ActionResult AddComment(Models.Comment commentToAdd)
+        {
+            commentToAdd.DateCreate = DateTime.Now;
+
+            //add comment to db
+            db.Comments.Add(commentToAdd);
+            db.SaveChanges();
+
+            //replace with AJAX
+            //return RedirectToAction("Index", "Home");
+            return PartialView("Comment", commentToAdd);
+
+        }
+        public ActionResult LikePost(int id)
+        {
+            Post postToLike = db.Posts.Find(id);
+            if (postToLike.Likes == null)
+            {
+                postToLike.Likes = 0;
+            }
+            postToLike.Likes++;
+            db.SaveChanges();
+            return Content(postToLike.Likes + " Likes");
+        }
+    }
+}
---
+++ MyBlog/MyBlog/Scripts/main.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function(){
+    $('.like').on('click', function () {
+        var postId = $(this).data('postid');
+        var theLikeButton = $(this);
+        $.get('/Home/LikePost/' + postId, function (data) {
+            theLikeButton.parent().html(data)
+        });
+    });
+
+    $('.showComments').on('click', function(){
+        var parent = $(this).parent();
+        var commentsToDisplay = parent.find('.commentsDiv');
+        commentsToDisplay.slideToggle();
+    });
+    //wiring up the Ajax Post for the comment form
+
+    $('.commentsDiv form').on('submit', function (event) {
+        event.preventDefault();
+        var theForm = $(this);
+        $.post(theForm.attr('action'), theForm.serialize(), function (data) {
+            //update our html
+            theForm.before(data);
+
+            theForm.find('.commentAuthor, .commentBody').val('')
+        });
+    });
+});
